837f09c0da06d1ab49fc13f45feee600c823f8b1
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2023-01-16 23:56:59 -0500

    Se crea repositorio para frontend con estilos base y componentes para inicio de maquetacion

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Buttons = () => {
+  return (
+    <section className='flexWrap'>
+    <button className="button"><span>Agregar</span></button>
+    <button className="button"><span>Editar</span></button>
+    <button className="button"><span>Buscar</span></button>
+    <button className="button"><span>Eliminar</span></button>
+    </section>
+  )
+}
+
+export default Buttons
---
+++ src/components/ClientTable.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+const ClientTable = () => {
+  return (
+<section className='TableContainer'>
+<table className='Table'>
+	<caption className=''>INFORMACIÓN DE CLIENTE</caption>
+	<thead>
+	<tr>
+		<th>Marca</th>
+		<th>Tipo De Id</th>
+		<th>Numero De Id</th>
+		<th>Primer Nombre</th>
+		<th>Segundo Nombre</th>
+		<th>Primer Apellido</th>
+		<th>Segundo Apellido</th>
+		<th>Fecha De Nacimiento</th>
+		<th>Pais</th>
+		<th>Departamento</th>
+		<th>Ciudad</th>
+		<th>Dirección</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	</tbody>
+</table>
+</section>
+  )
+}
+
+export default ClientTable
